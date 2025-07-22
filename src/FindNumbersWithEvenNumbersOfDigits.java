d34d4648be3eea56b88d0ae80ad5c539bbffdb57
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-07-22 13:55:02 +0530

    Adding the first program of LeetCode the Find Numbers With Even Numbers Of Digits

---
+++ src/FindNumbersWithEvenNumbersOfDigits.java
@@ -0,0 +1,27 @@
+public class FindNumbersWithEvenNumbersOfDigits {
+
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums){
+            if(even(num)){
+                count ++;
+            }
+        }
+
+        return count;
+    }
+
+    public static boolean even(int num){
+
+        int numberOfDigits = (int) Math.log10(num) + 1;
+
+        return numberOfDigits % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {12, 344, 34431, 53223, 4567};
+
+        System.out.println(findNumbers(nums));
+    }
+
+}
