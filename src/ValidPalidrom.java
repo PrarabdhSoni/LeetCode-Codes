d3eb9fa56ebb28145a3ca3b5a3c00bc87a6439d0
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-07-29 22:52:57 +0530

    Adding Valid Palidrom program

---
+++ src/ValidPalidrom.java
@@ -0,0 +1,40 @@
+import java.lang.Character;
+
+public class ValidPalidrom {
+
+        public static boolean isPalindrome(String s) {
+
+            int start = 0;
+            int end = s.length() -1;
+
+            while(start <= end){
+                char ch1 = s.charAt(start);
+                char ch2 = s.charAt(end);
+
+                if(!(Character.isLetterOrDigit(ch1))){
+                    start ++;
+                    continue;
+                }
+
+                if(!(Character.isLetterOrDigit(ch2))){
+                    end --;
+                    continue;
+                }
+
+                if(Character.toLowerCase(ch1) != Character.toLowerCase(ch2)){
+                    return false;
+                }
+
+                start ++;
+                end --;
+
+            }
+
+            return true;
+
+        }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+}
